7af39aeb1c450c2f6b4c7eab7bf17a7a08fea0d9
biyro02 <biyro2@gmail.com>
2018-10-14 11:00:41 +0200

    sdasd

---
+++ src/com/vektorel/group38/les2/Aritmethic.java
@@ -0,0 +1,38 @@
+package com.vektorel.group38.les2;
+
+/*
+
+Ctrl+Alt+L  Kodu formatlamak için
+
+ */
+public class Aritmethic {
+
+
+    public void toplama(int sayi1, int sayi2) {
+        // 5 + 7 = 12
+
+        int toplam = sayi1 + sayi2;
+        System.out.println("Toplam : " + toplam);
+
+        // System.out.println(sayi1 + sayi2);
+    }
+
+    public void cikarma(int sayi1, int sayi2) {
+        int fark = sayi1 - sayi2;
+
+        System.out.println("Fark : " + fark);
+    }
+
+    public void carpma(int sayi1, int sayi2) {
+        int carpim = sayi1 * sayi2;
+
+        System.out.println("Çarpım : " + carpim);
+    }
+
+    public void bolme(int sayi1, int sayi2) {
+        int bolum = sayi1 / sayi2;
+
+        System.out.println("Bolum : " + bolum);
+    }
+
+}
---
+++ src/com/vektorel/group38/les2/Runner.java
@@ -0,0 +1,19 @@
+package com.vektorel.group38.les2;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        // Instance yaratılıyor (veya nesnesi yaratılıyor)
+        Aritmethic aritmethic = new Aritmethic();
+
+        aritmethic.bolme(34, 5);
+
+        aritmethic.carpma(45, 44);
+
+        aritmethic.cikarma(234, 345);
+
+        aritmethic.toplama(234, 345);
+
+
+    }
+}
