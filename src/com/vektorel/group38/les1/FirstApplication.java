674c5d4ad8a074c31174e53768fe7038d7a40718
biyro02 <biyro2@gmail.com>
2018-10-14 10:18:06 +0200

    First Initialization 2

--- src/com/vektorel/group38/les1/FirstApplication.java
+++ src/com/vektorel/group38/les1/FirstApplication.java
@@ -1,8 +1,20 @@
 package com.vektorel.group38.les1;
 
+
+import com.vektorel.group38.les2.Lesson2;
+
 public class FirstApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World Application");
+
+        Lesson2 lesson2 =new Lesson2();
+        lesson2.exampleMethod();
+
+        lesson2.exampleMethod(45);
+
+        lesson2.exampleMethod("Ahmet" );
+
     }
+
 }
