8b636bf8317f966c3f12ca5d8fa4fe1e5f3116be
biyro02 <biyro2@gmail.com>
2018-10-14 09:56:01 +0200

    First Initialization

---
+++ src/com/vektorel/group38/les1/FirstApplication.java
@@ -0,0 +1,8 @@
+package com.vektorel.group38.les1;
+
+public class FirstApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Application");
+    }
+}
---
+++ src/com/vektorel/group38/les2/Lesson2.java
@@ -0,0 +1,16 @@
+package com.vektorel.group38.les2;
+
+public class Lesson2 {
+
+    public void exampleMethod(){
+        System.out.println("Hi");
+    }
+
+    public void exampleMethod(String content){
+        System.out.println(content);
+    }
+
+    public void exampleMethod(int age){
+        System.out.println("Your age is " + age);
+    }
+}
